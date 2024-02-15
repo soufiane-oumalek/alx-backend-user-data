9b3dcc0ec3c8846feb59ee0057756823d5e28173
soufiane-oumalek <soufian.oumalek@gmail.com>
2024-02-16 00:34:16 +0100

    editing_basic_auth

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -4,6 +4,7 @@ from api.v1.views import app_views
 from flask import abort, jsonify, request
 from models.user import User
 from os import getenv
+import os
 
 
 @app_views.route('/auth_session/login', methods=['POST'], strict_slashes=False)
