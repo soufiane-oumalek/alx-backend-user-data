644604a26ffe00e9c9c5ee9a77fdf263fc3b9516
soufiane-oumalek <soufian.oumalek@gmail.com>
2024-02-16 00:22:21 +0100

    editing_basic_auth

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -46,21 +46,21 @@ def not_found(error) -> str:
 
 @app.before_request
 def before_request() -> str:
-    """ Before request handler
-    """
-    if auth is not None:
-        if auth.require_auth(
-                request.path,
-                ['/api/v1/status/',
-                    '/api/v1/unauthorized/',
-                    '/api/v1/forbidden/',
-                    '/api/v1/auth_session/login/']):
-            if auth.authorization_header(request) is None and \
-                    auth.session_cookie(request) is None:
-                abort(401)
-            request.current_user = auth.current_user(request)
-            if request.current_user is None:
-                abort(403)
+    """ method to handler before request """
+    if auth is None:
+        return
+    excluded_paths = ['/api/v1/status/',
+                      '/api/v1/unauthorized/',
+                      '/api/v1/forbidden/',
+                      '/api/v1/auth_session/login/']
+    if not auth.require_auth(request.path, excluded_paths):
+        return
+    if not auth.authorization_header(request)\
+       and not auth.session_cookie(request):
+        abort(401)
+    if auth.current_user(request) is None:
+        abort(403)
+    request.current_user = auth.current_user(request)
 
 
 if __name__ == "__main__":
