bb5b784f77352a1ab9bfe1c1dc522c90828a929c
soufiane-oumalek <soufian.oumalek@gmail.com>
2024-02-16 00:35:26 +0100

    editing_basic_auth

--- 0x02-Session_authentication/api/v1/auth/auth.py
+++ 0x02-Session_authentication/api/v1/auth/auth.py
@@ -1,49 +1,49 @@
 #!/usr/bin/env python3
+""" Module session_auth
 """
-Authentication class
-"""
-
-from tabnanny import check
-from flask import request
-from typing import TypeVar, List
-User = TypeVar('User')
+from api.v1.views import app_views
+from flask import abort, jsonify, request
+from models.user import User
+from typing import TypeVar
+import os
 
 
-class Auth:
+@app_views.route('/auth_session/login', methods=['POST'], strict_slashes=False)
+def login() -> str:
+    """ POST /api/v1/auth_session/login
+    Return:
+      - User object JSON represented
+      - 400 if the User doesn't exist
     """
-    class API authentication
+    email = request.form.get("email")
+    password = request.form.get("password")
+    if email is None or email == "":
+        return jsonify({"error": "email missing"}), 400
+    if password is None or password == "":
+        return jsonify({"error": "password missing"}), 400
+    user = User.search({"email": email})
+    if user is None or user == []:
+        return jsonify({"error": "no user found for this email"}), 404
+    user = user[0]
+    if not user.is_valid_password(password):
+        return jsonify({"error": "wrong password"}), 401
+    from api.v1.app import auth
+    session_id = auth.create_session(user.id)
+    response = jsonify(user.to_json())
+    response.set_cookie(os.getenv("SESSION_NAME"), session_id)
+    return response
+
+
+@app_views.route(
+        '/auth_session/logout',
+        methods=['DELETE'],
+        strict_slashes=False)
+def logout() -> str:
+    """ DELETE /api/v1/auth_session/logout
+    Return:
+      - empty JSON if the session has been correctly deleted
     """
-
-    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        """
-        authentication method
-        """
-        check = path
-        if path is None or excluded_paths is None or len(excluded_paths) == 0:
-            return True
-        if path[-1] != "/":
-            check += "/"
-        if check in excluded_paths or path in excluded_paths:
-            return False
-        return True
-
-    def authorization_header(self, request=None) -> str:
-        """
-        authorization method
-        """
-        if request is None:
-            return None
-        return request.headers.get("Authorization")
-
-    def current_user(self, request=None) -> User:
-        """
-        current method
-        """
-        return None
-
-    def session_cookie(self, request=None):
-        """ returns a request cookie  """
-        if request is None:
-            return None
-        session = getenv('SESSION_NAME')
-        return request.cookies.get(session)
+    from api.v1.app import auth
+    if auth.destroy_session(request) is False:
+        abort(404)
+    return jsonify({}), 200
