6c14a0d37d0b7e17c9b235aa3d655675a792f021
soufiane-oumalek <soufian.oumalek@gmail.com>
2024-02-16 00:00:59 +0100

    editing_basic_auth

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -7,9 +7,6 @@ from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
 import os
-from api.v1.auth.auth import Auth
-from api.v1.auth.basic_auth import BasicAuth
-from api.v1.auth.session_auth import SessionAuth
 
 
 app = Flask(__name__)
@@ -18,10 +15,13 @@ CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 auth = None
 AUTH_TYPE = getenv("AUTH_TYPE")
 if AUTH_TYPE == 'auth':
+    from api.v1.auth.auth import Auth
     auth = Auth()
 elif AUTH_TYPE == 'basic_auth':
+    from api.v1.auth.basic_auth import BasicAuth
     auth = BasicAuth()
 elif AUTH_TYPE == "session_auth":
+    from api.v1.auth.session_auth import SessionAuth
     auth = SessionAuth()
 
 
