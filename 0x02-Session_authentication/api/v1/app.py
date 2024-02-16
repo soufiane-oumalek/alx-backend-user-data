8af8552cf58e3668035ee4614874d93678bc899c
soufiane-oumalek <soufian.oumalek@gmail.com>
2024-02-16 02:46:41 +0100

    editing_basic_auth

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -40,9 +40,9 @@ def run_before_each_request():
     if auth is None:
         return
     excluded_paths = ['/api/v1/status/',
-             '/api/v1/unauthorized/',
-             '/api/v1/forbidden/',
-             '/api/v1/auth_session/login/']
+                      '/api/v1/unauthorized/',
+                      '/api/v1/forbidden/',
+                      '/api/v1/auth_session/login/']
     if not auth.require_auth(request.path, excluded_paths):
         return
     if auth.authorization_header(request) is None and\
