bc16bf3ef7abff9f281e947ff14fb66e188898f3
soufiane-oumalek <soufian.oumalek@gmail.com>
2024-02-21 00:15:49 +0100

    user_auth_serv

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -30,7 +30,6 @@ class DB:
             self.__session = DBSession()
         return self.__session
 
-
     def add_user(self, email: str, hashed_password: str) -> User:
         """ add user """
         user = User(email=email, hashed_password=hashed_password)
