17da6b4d583b7062b2bf8e06838221c812b07046
soufiane-oumalek <soufian.oumalek@gmail.com>
2024-02-22 15:36:48 +0100

    user_auth_serv

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
 """ Hash password """
-import bcrypt
-from sqlalchemy.orm.exc import NoResultFound
-import uuid
 from db import DB
+import bcrypt
+import uuid
+from typing import Union, TypeVar
 from user import User
+from sqlalchemy.orm.exc import NoResultFound
 
 
 def _hash_password(password: str) -> str:
@@ -76,18 +77,14 @@ class Auth:
             return None
 
     def get_reset_password_token(self, email: str) -> str:
-        """ reset password token for a user.
-        """
-        reset_user = None
+        """ reset password token """
         try:
-            reset_user = self._db.find_user_by(email=email)
+            res_user = self._db.find_user_by(email=email)
         except NoResultFound:
-            reset_user = None
-        if reset_user is None:
-            raise ValueError()
-        token = _generate_uuid()
-        self._db.update_user(reset_user.id, reset_token=token)
-        return token
+            raise ValueError
+        tkn = _generate_uuid()
+        self._db.update_user(res_user.id, reset_token=tkn)
+        return tkn
 
     def update_password(self, reset_token: str, password: str) -> None:
         """ update password """
